a2643169af26b247f5077a0b4e5aaf22a85f318a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-03-02 09:40:12 +0000

    Add basic camera view

--- src/frontend/components/AppContainer.js
+++ src/frontend/components/AppContainer.js
@@ -1,21 +1,10 @@
-import React from "react";
-import { Text, View } from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import LocationScreen from "./LocationScreen";
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
+import CameraView from "./CameraView";
 
 const TabNavigator = createBottomTabNavigator({
   Home: LocationScreen,
-  Settings: SettingsScreen
+  Settings: CameraView
 });
 
 export default createAppContainer(TabNavigator);
